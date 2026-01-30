9d824a9b68de9b890d1e419bc523101cde8bff6f
mohamed-710 <moha2023toedu@gmail.com>
2026-01-30 18:19:37 +0200

    add category

--- middlewares/fileUpload.js
+++ middlewares/fileUpload.js
@@ -1,24 +1,43 @@
 import multer, { diskStorage } from 'multer';
-import appError from '../utils/AppError.js';
+import appError from '../utils/appError.js';
 import httpStatusText from '../utils/httpStatusText.js';
-
+import fileType from "file-type";
+import fs from 'fs/promises';
 const uploadFileCloud = () => {
 
     const photostorage = diskStorage({}); //save file in system "temp"
-    const fileFilter = (req, file, cb) => {
+
+    const fileFilter =(req, file, cb) => {
         if (!file.mimetype.startsWith('image/')) {
             return cb(
-                appError.create('File must be an image', 400, httpStatusText.FAIL),
+                appError.create
+                    ('File must be an image', 400, httpStatusText.FAIL),
                 false
             );
         }
         return cb(null, true);
-    };
+    };  
     const multerUpload = multer({
         storage: photostorage,
         fileFilter,
+        limits: {
+            fileSize: 5 * 1024 * 1024, // 5MB limit
+        }
     });
     return multerUpload;
 };
+const validateUploadedImage = async (filePath) => {
+    const buffer = await fs.readFile(filePath);
+    const type = await fileType.fromBuffer(buffer);
+    
+    if (!type || !type.mime.startsWith('image/')) {
+        // Delete the invalid file
+        await fs.unlink(filePath);
+        throw appError.create('Invalid file signature', 400, httpStatusText.FAIL);
+    }
+    
+    return type;
+};
 
-export { uploadFileCloud };
+
+export { uploadFileCloud ,validateUploadedImage};
---
+++ models/Category.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+const categorySchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true,
+        min:5,
+        max:20
+    },
+    slug:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    createdBy:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    image:{
+        id:{type:String},
+        secure_url:{type:String}
+    },
+},
+{timestamps:true}
+);
+const Category=mongoose.model("Category",categorySchema);
+export default Category;
