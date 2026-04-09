2a6cf02be3b0c90840b757cb2ad366991b072644
mohamed-710 <moha2023toedu@gmail.com>
2026-04-09 07:50:36 +0200

    update cart controller

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -4,13 +4,13 @@ import appError from '../utils/appError.js';
 import httpStatusText from '../utils/httpStatusText.js';
 import Product from "../models/product.js";
 export const addTocart = asyncWrapper(async (req, res, next) => {
-    const {productId,quantity}=req.body;
+    const { productId, quantity } = req.body;
 
-    const product=await Product.findById(productId);
+    const product = await Product.findById(productId);
 
-    if(!product) return next(appError.create("product not found", 404, httpStatusText.FAIL));
+    if (!product) return next(appError.create("product not found", 404, httpStatusText.FAIL));
 
-    if(product.availableStock<quantity) return next(appError.create("out of stock", 400, httpStatusText.FAIL));
+    if (product.availableStock < quantity) return next(appError.create("out of stock", 400, httpStatusText.FAIL));
     //@TODO check if product already in cart and update quantity
     const cart = await Cart.findOneAndUpdate(
         {
@@ -26,8 +26,8 @@ export const addTocart = asyncWrapper(async (req, res, next) => {
                 }
             }
         },
-            { new: true }
-        );
+        { new: true }
+    );
     return res.json({
         success: true,
         results: cart
@@ -44,27 +44,27 @@ export const getCart = asyncWrapper(async (req, res, next) => {
         });
     };
     if (req.user.role === 'admin' && !req.body.cartId) return next(appError.create("cartId is required for admin", 400, httpStatusText.FAIL));
-        const cart = await Cart.findById(req.body.cartId);
-        if (!cart) return next(appError.create("cart not found", 404, httpStatusText.FAIL));
-        return res.json({
-            success: true,
-            results: {cart}
-        });
-}) 
+    const cart = await Cart.findById(req.body.cartId);
+    if (!cart) return next(appError.create("cart not found", 404, httpStatusText.FAIL));
+    return res.json({
+        success: true,
+        results: { cart }
+    });
+})
 
-export const updateCart=(async(req,res,next)=>{
-    const {productId,quantity}=req.body;
-    const cart=await Cart.findOneAndUpdate(
+export const updateCart = (async (req, res, next) => {
+    const { productId, quantity } = req.body;
+    const cart = await Cart.findOneAndUpdate(
         {
-            user:req.user._id,
-            "items.productId":productId        
+            user: req.user._id,
+            "items.productId": productId
         },
-        {"items.$.quantity":quantity},
-        {new:true}
+        { "items.$.quantity": quantity },
+        { new: true }
     );
     return res.json({
-        success:true,
-        results:cart
+        success: true,
+        results: cart
     })
 })
 
